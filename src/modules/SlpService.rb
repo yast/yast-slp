70af9534f5571706c7010cfe27412e624a0ef54a
Vladimir Moravec <vmoravec@suse.com>
2014-01-30 11:28:51 +0100

    Create SlpService module and add specs

---
+++ src/modules/SlpService.rb
@@ -0,0 +1,128 @@
+require 'yast'
+require 'resolv'
+require 'ostruct'
+
+module Yast
+  Yast.import 'SLP'
+
+  class SlpServiceClass < Module
+
+    SCHEME = 'service'
+    DELIMITER = ':'
+
+    def find service_name, params={}
+      service = nil
+      service_type = [SCHEME, service_name, params[:protocol]].compact.join(DELIMITER)
+      discover_service(service_type, params[:scope]).each do |slp_response|
+        service = Service.new(service_name, slp_response, params)
+        service = service.match(params)
+        break if service
+      end
+      service
+    end
+
+    def all service_name, params={}
+      service_type = [SCHEME, service_name, params[:protocol]].compact.join(DELIMITER)
+      discover_service(service_type, params[:scope]).map do |slp_response|
+        Service.new(service_name, slp_response, params).match(params)
+      end.compact
+    end
+
+    def types
+      available_services = []
+      discovered_services = discover_available_slp_service_types
+      return available_services if discovered_services.empty?
+
+      discovered_services.each do |slp_service_type|
+        available_services << parse_slp_type(slp_service_type)
+      end
+      available_services
+    end
+
+    alias_method :keys, :types
+
+    private
+
+    def parse_slp_type service_type
+      type_parts = service_type.split(DELIMITER)
+      case type_parts.size
+      when 2
+        name = protocol = type_parts.last
+      when 3
+        name = type_parts[1]
+        protocol = type_parts[2]
+      else
+        raise "Incorrect slp service type: #{service.inspect}"
+      end
+      OpenStruct.new :name => name, :protocol => protocol
+    end
+
+    def discover_service service_name, scope=''
+      SLP.FindSrvs(service_name, scope)
+    end
+
+    def discover_available_slp_service_types
+      SLP.FindSrvTypes('*', '')
+    end
+
+    class Service
+
+      attr_reader :name, :ip, :host, :protocol, :port
+      attr_reader :slp_type, :slp_url, :lifetime, :attributes
+
+      def initialize service_name, slp_data, params
+        @name = service_name
+        @ip = slp_data['ip']
+        @port = slp_data['pcPort']
+        @slp_type = slp_data['pcSrvType']
+        @slp_url = slp_data['srvurl']
+        @protocol = params[:protocol] || slp_type.split(DELIMITER).last
+        @host = resolve_host
+        @lifetime = slp_data['lifetime']
+        @attributes = OpenStruct.new(SLP.GetUnicastAttrMap(slp_url, ip))
+      end
+
+      def match params
+        matches = []
+        params.each do |key, value|
+          if respond_to?(key)
+            result = send(key).to_s
+            matches << result.match(/#{value}/i)
+          elsif attributes.respond_to?(key)
+            result = attributes.send(key).to_s
+            matches << result.match(/#{value}/i)
+          else
+            return
+          end
+        end
+        matches.all? ? self : nil
+      end
+
+      private
+
+      def resolve_host
+        host = DnsCache.find(ip)
+        return host if host
+
+        host = Resolv.getname(ip)
+        DnsCache.update(ip => host)
+        host
+      end
+
+      module DnsCache
+        def self.entries
+          @entries ||= {}
+        end
+
+        def self.find ip_address
+          entries[ip_address]
+        end
+
+        def self.update entry
+          entries.merge!(entry)
+        end
+      end
+    end
+  end
+  SlpService = SlpServiceClass.new
+end
---
+++ test/slp_service_test.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env rspec
+#
+ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+
+require "yast"
+
+Yast.import 'SlpService'
+
+describe Yast::SlpService do
+  before do
+    Yast::SlpService.stub(:discover_service).and_return(
+      [
+        {
+          'ip'        => '10.100.2.16',
+          'pcFamily'  => 'IP',
+          'pcHost'    => '10.100.2.16',
+          'pcPort'    => 0,
+          'pcSrvPart' => '/install/SLP/SLE-10-SP4-SDK-RC3/x86_64/DVD1',
+          'pcSrvType' => 'service:install.suse.http',
+          'srvurl'    => 'service:install.suse:http://10.100.2.16/install/SLP/SLE-10-SP4-SDK-RC3/x86_64/DVD1',
+          'lifetime'  => 65535
+        }
+      ]
+    )
+
+    SLP.stub(:GetUnicastAttrMap).and_return(
+      {
+        'machine' => 'x86_64',
+        'description' => 'SLE_10_SP4_SDK'
+      }
+    )
+
+    SLP.stub(:FindSrvTypes).and_return(
+      [
+        "service:smtp",
+        "service:install.suse:http",
+        "service:ntp",
+        "service:ldap"
+      ]
+    )
+
+    ::Resolv.stub(:getname).and_return('fallback.suse.cz')
+  end
+
+  describe "#find" do
+    it "returns the first discovered service that matches the service name and params" do
+      service = Yast::SlpService.find('install.suse', :protocol=>'http', :machine=>'x86_64')
+      expect(service.name).to eq('install.suse')
+      expect(service.ip).to eq('10.100.2.16')
+      expect(service.host).to eq('fallback.suse.cz')
+      expect(service.protocol).to eq('http')
+      expect(service.port).to eq(0)
+      expect(service.lifetime).to eq(65535)
+      expect(service).to respond_to(:attributes)
+      expect(service.attributes).to respond_to(:machine)
+      expect(service.attributes.machine).to eq('x86_64')
+      expect(service.attributes).to respond_to(:description)
+      expect(service.attributes.description).to eq('SLE_10_SP4_SDK')
+    end
+  end
+
+  describe "#all" do
+    it "returns a collection of services" do
+      services = Yast::SlpService.find('install.suse')
+      expect(services.size).to eq(1)
+      expect(services.first).to be_a(Yast::SlpService)
+    end
+  end
+
+  describe "#types" do
+    it "returns a collection of discovered services" do
+      service_types = Yast::SlpService.types
+      expect(service_types).to respond_to(:each)
+      type = service_types.find {|t| t.name == 'install.suse' }
+      expect(type).not_to eq(nil)
+      expect(type.name).to eq('install.suse')
+      expect(type.protocol).to eq('http')
+    end
+  end
+end
