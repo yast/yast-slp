ab5661509f6a01f78eca65f1bb473a1eb3425db3
Imobach González Sosa <igonzalezsosa@suse.com>
2016-07-25 10:15:03 +0100

    Add the spec_helper to tests/

---
+++ test/spec_helper.rb
@@ -0,0 +1,37 @@
+ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+
+require "yast"
+require "pathname"
+require "yast/rspec"
+
+if ENV["COVERAGE"]
+  require "simplecov"
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+
+  SimpleCov.start do
+    add_filter "/test/"
+  end
+end
+
+# configure RSpec
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    # https://relishapp.com/rspec/rspec-mocks/v/3-0/docs/verifying-doubles/partial-doubles
+    c.verify_partial_doubles = true
+  end
+
+  config.extend Yast::I18n # available in context/describe
+  config.include Yast::I18n # available in it/let/before
+end
+
+libdir = File.expand_path("../../src/lib", __FILE__)
+$LOAD_PATH.unshift(libdir)
