6f7b806dabec70f495438fc7aea958e099dd78dd
Josef Reidinger <jreidinger@suse.cz>
2021-01-14 23:05:57 +0100

    add github actions

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -8,19 +8,29 @@ require "yast/rspec"
 
 if ENV["COVERAGE"]
   require "simplecov"
+  SimpleCov.start do
+    add_filter "/test/"
+  end
 
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
+  src_location = File.expand_path("../src", __dir__)
+  # track all ruby files under src
+  SimpleCov.track_files("#{src_location}/**/*.rb")
+
+  # additionally use the LCOV format for on-line code coverage reporting at CI
+  if ENV["CI"] || ENV["COVERAGE_LCOV"]
+    require "simplecov-lcov"
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      # this is the default Coveralls GitHub Action location
+      # https://github.com/marketplace/actions/coveralls-github-action
+      c.single_report_path = "coverage/lcov.info"
+    end
 
     SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
       SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
+      SimpleCov::Formatter::LcovFormatter
     ]
-  end
-
-  SimpleCov.start do
-    add_filter "/test/"
   end
 end
 
